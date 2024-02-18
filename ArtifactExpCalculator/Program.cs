221995c3fc534b33745e520a6d6b56ce91d0acfc
usernameAki <akisklep@gmail.com>
2024-02-18 23:10:11 +0100

    Fixed bugs

--- ArtifactExpCalculator/Program.cs
+++ ArtifactExpCalculator/Program.cs
@@ -8,21 +8,55 @@ namespace ArtifactExpCalculator
         const int FourStarExp = 2520;
         const int FiveStarExp = 3780;
         static Random rnd = new Random();
+        static int notFilledSlots = 0;
         static void Main(string[] args)
         {
+            //1.famimy artefakty wykorzystując 180 resina.
+            //2.Przeprowadzamy ulepszanie wykorzystując 15 oraz 1 slot.
+            //3.Sumujemy otrzymany exp.
+            var xpCalc = new XPCalc();
+            int iterations = 10000;
+            for(int days = 1; days <= 365; days++)
+            {
+                double difference = 0;
+                long Exp1 = 0;
+                long Exp2 = 0;
+                for (int i = 0; i < iterations; i++)
+                {
+                    long totalExp1 = 0;
+                    long totalExp2 = 0;
+                    for (int a = 0; a < 1; a++)
+                    {
+                        var artifacts = xpCalc.FarmDomainForArtifacts(180 * days);
+                        totalExp1 += xpCalc.Upgrade(artifacts, 15);
+                        totalExp2 += xpCalc.Upgrade(artifacts, 1);
+                    }
+                    difference += CalculateDifference(totalExp1, totalExp2);
+                    Exp1 += totalExp1;
+                    Exp2 += totalExp2;
+                }
+                var result = difference / iterations;
+                Console.WriteLine("Bulk xp: " + Exp1);
+                Console.WriteLine("Unit xp: " + Exp2);
+                Console.WriteLine("Difference: " + result);
+            }
+        }
+        static void StartSimulation()
+        {
             double sum = 0;
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 var exp = CalculateArtifactExp();
                 var difference = CalculateDifference(exp[0], exp[1]);
-                sum += difference; 
+                sum += difference;
             }
-            Console.WriteLine($"Difference: {sum / 10000}.");
+            Console.WriteLine($"Difference: {sum / 1000}.");
+            Console.WriteLine($"Not filled slots: {notFilledSlots}.");
         }
         static int[] CalculateArtifactExp() //for 1 day
         {
             var artifacts = new int[3];
-            for (int i = 0; i < 9; i++) // How many times you do domain.
+            for (int i = 0; i < 9; i++) // How many times you do domain. 9x 20 resin
             {
                 var loot = LootArtifacts();
                 artifacts[0] += loot[0];
@@ -30,7 +64,7 @@ namespace ArtifactExpCalculator
                 artifacts[2] += loot[2];
             }
             var expGot = new int[2];
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 int bulkExp = Upgrade(artifacts, 15);
                 int unitExp = Upgrade(artifacts, 1);
@@ -41,27 +75,27 @@ namespace ArtifactExpCalculator
             //Console.WriteLine($"Total exp from unit {expGot[1]}.");
             return expGot;
         }
-        static double CalculateDifference(int value1, int value2)
+        static double CalculateDifference(long value1, long value2)
         {
-            int bigger;
-            int smaller;
+            double bigger;
+            double smaller;
             if (value1 > value2)
             {
                 bigger = value1;
                 smaller = value2;
             }
-            else 
+            else if (value1 < value2)
             { 
                 bigger = value2;
                 smaller = value1; 
-            }
-            double result = (bigger - smaller) / (smaller / 100);
+            } else return 0;
+            double result = (bigger - smaller) / smaller * 100;
             return result;
         }
         static int Upgrade(int[] artifacts, int artifactPool)
         {
             int[] usedArtifacts = new int[2];
-            int expPool = 0;
+            int expPool;
             int expTotal = 0;
             while (artifacts[0] != usedArtifacts[0]
                 && artifacts[1] != usedArtifacts[1])
@@ -83,13 +117,15 @@ namespace ArtifactExpCalculator
                 }
                 expTotal += expPool * CalculateBonus();
             }
+            var multiplier = (artifacts[0] + artifacts[1]) / artifactPool;
+            notFilledSlots += (15 - artifactPool) * multiplier;
             return expTotal;
         }
         static int CalculateBonus()
         {
             var rndResult = rnd.Next(0, 100);
-            if (rndResult <= 90) return 1;
-            else if (rndResult > 90 && rndResult <= 98) return 2;
+            if (rndResult < 90) return 1;
+            else if (rndResult >= 90 && rndResult <= 98) return 2;
             else if (rndResult == 99) return 5;
             else
             {
@@ -99,7 +135,6 @@ namespace ArtifactExpCalculator
         }
         static int[] LootArtifacts() //[0] = 3*,[1] = 4*,[2] = 5*,
         {
-            var rnd = new Random();
             int[] result = new int[3] { 3, 2, 1 };
             int rndResult = rnd.Next(0,1000);
             result[0] = rndResult <= 549 ? result[0]++ : result[0];
---
+++ ArtifactExpCalculator/XPCalc.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtifactExpCalculator
+{
+    public class XPCalc
+    {
+        Random rnd = new Random();
+        const int ThreeStarExp = 1260;
+        const int FourStarExp = 2520;
+        public int StartSimulation(int[] artifacts, int artifactPool)
+        {
+            var result = Upgrade(artifacts, artifactPool);
+            return result;
+        }
+        public int[] FarmDomainForArtifacts(int resin)
+        {
+            int iterations = resin / 20;
+            int[] result = new int[2] { 3* iterations, 2*iterations };
+            int rndResult;
+            for (int i = 0; i < iterations; i++)
+            {
+                rndResult = rnd.Next(0, 1000);
+                result[0] = rndResult <= 549 ? result[0] + 1 : result[0];
+                rndResult = rnd.Next(0, 1000);
+                result[1] = rndResult <= 484 ? result[1] + 1 : result[1];
+            }
+            return result;
+        }
+        /// <returns>Returns Total XP from Upgrade process.</returns>
+        public int Upgrade(int[] artifacts, int artifactPool)
+        {
+            int[] usedArtifacts = new int[2] {0,0};
+            int expPool;
+            int expTotal = 0;
+            while (artifacts[0] != usedArtifacts[0]
+                && artifacts[1] != usedArtifacts[1])
+            {
+                expPool = 0;
+                for (int i = 0; i < artifactPool; i++)
+                {
+                    if (artifacts[1] != usedArtifacts[1])
+                    {
+                        expPool += FourStarExp;
+                        usedArtifacts[1]++;
+                    }
+                    else if (artifacts[0] != usedArtifacts[0])
+                    {
+                        expPool += ThreeStarExp;
+                        usedArtifacts[0]++;
+                    }
+                    else
+                    {
+                        expPool = 0;
+                        break;
+                    }
+                }
+                expTotal += expPool * BonusXP();
+            }
+            return expTotal;
+        }
+        private int BonusXP()
+        {
+            var rndResult = rnd.Next(0, 100);
+            if (rndResult < 90) return 1;
+            else if (rndResult >= 90 && rndResult <= 98) return 2;
+            else if (rndResult == 99) return 5;
+            else
+            {
+                Console.WriteLine($"Error in CalculateBonus. Wrong Number{rndResult}. Returned 0.");
+                return 0;
+            }
+        }
+    }
+}
